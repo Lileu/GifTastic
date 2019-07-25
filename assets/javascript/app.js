a17ed1e9da4e09e276700534cd7726dc5a10be4b
Lisa Leu <Lisal@netwealth.com.au>
2019-07-25 14:54:08 +1000

    final js commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,10 @@
-$(document).ready(function () { 
+$(document).ready(function () {
 
     // Initial array of topics
     var topics = ["excitement", "yasssss", "happy dance", "awesome", "sad", "gag", "cute", "sorry", "high five", "bummer", "dabbing"];
     console.log(topics);
-
+    
+    // Render them buttons
     renderButtons();
 
     // Function to render buttons from the topics array
@@ -19,65 +20,66 @@ $(document).ready(function () {
             $("#buttons-section").append(btn);
         }
     }
-    // Render them buttons
     // On-click Jquery function to push the submitted text into the topics array
-    $("#submitBtn").on('click',function (event) {
-            event.preventDefault();
-            var topic = $("#addTopic").val().trim();
-            if (topic != "") {
-                topics.push(topic);
-                $("#addTopic").val('');
-                renderButtons();
-            }
-        });
+    $("#submitBtn").on('click', function (event) {
+        event.preventDefault();
+        var topic = $("#addTopic").val().trim();
+        if (topic != "") {
+            topics.push(topic);
+            $("#addTopic").val('');
+            renderButtons();
+        }
+    });
 
+    // Click event to retrieve gifs from topic
+    $(document).on('click', 'button', function () {
+        // Clear section
+        $("#gifs-section").empty();
+        // Generate api query from data attribute of button clicked 
+        var b = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?&api_key=3HskJVbTCA0hiHZGsNyb1jQ3Qh3U9vjf&q=" + b + "&limit=10&lang=en";
+        console.log(queryURL);
 
-// Click event to retrieve related gifs
-$(document).on('click', 'button', function () {
-    // Clear section
-    $("#gifs-section").empty();
-    // Generate api query from data attribute of button clicked 
-    var b = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?&api_key=3HskJVbTCA0hiHZGsNyb1jQ3Qh3U9vjf&q=" + b + "&limit=10&lang=en";
-    console.log(queryURL);
+        // GET request ajax call
+        $.ajax({
+                url: queryURL,
+                method: "GET",
+            })
+            .done(function (response) {
+                console.log(queryURL);
+                console.log(response);
+                // Store array of results from the response
+                var results = response.data;
+                // For-loop to display gifs and required information
+                for (i = 0; i < results.length; i++) {
+                    var gifDiv = $('<div class="imgDiv">');
+                    var p = $("<p>").text("Rating: " + results[i].rating);
+                    var gifImage = $("<img>");
+                    // Set up gif data attributes
+                    gifImage.attr("src", results[i].images.fixed_height_still.url)
+                        .attr('data-animated', results[i].images.fixed_height.url)
+                        .attr('data-still', results[i].images.fixed_height_still.url)
+                        .attr('data-state', "still")
+                        .addClass("showImage");
+                    // Append the paragraph and image tag to the gifDiv
+                    gifDiv.append(p);
+                    gifDiv.append(gifImage);
+                    $("#gifs-section").prepend(gifDiv);
+                };
+                // On-click function to toggle between still and animated gif states
+                $(document).on('click', '.showImage', function () {
+                    var state = $(this).attr('data-state');
+                    console.log(state);
+                    if (state == "still") {
+                        $(this).attr("data-state", "animated");
+                        $(this).attr("src", $(this).attr("data-animated"));
+                    } else {
+                        $(this).attr("data-state", "still");
+                        $(this).attr("src", $(this).attr("data-still"));
+                    }
 
-    // GET request ajax call
-    $.ajax({
-            url: queryURL,
-            method: "GET",
-        })
-        .done(function (response) {
-            console.log(queryURL);
-            console.log(response);
-            // Store array of results from the response
-            var results = response.data;
-            // For-loop to display gifs and required information
-            for (i = 0; i < results.length; i++) {
-                var gifDiv = $('<div class="imgDiv">');
-                var p = $("<p>").text("Rating: " + results[i].rating);
-                var gifImage = $("<img>");
-                // Set up data attributes
-                gifImage.attr("src", results[i].images.fixed_height.url)
-                    .attr('data-still', results[i].images.fixed_height_still.url)
-                    .attr('data-animate', results[i].images.fixed_height.url)
-                    .attr('data-state', "still")
-                    .addClass("showImage");
-                // Append the paragraph and image tag to the gifDiv
-                gifDiv.append(p);
-                gifDiv.append(gifImage);
-                $("#gifs-section").prepend(gifDiv);
-            }
-        })
-});
+                });
 
-$(document).on('click', '.showImage', function () {
-    var state = $(this).data('state');
-    if (state == "still") {
-        $(this).attr("data-state", "animated");
-        $(this).attr("src", $(this).attr("data-animated"));
-    } else {
-        $(this).attr("data-state", "still");
-        $(this).attr("src", $(this).attr("data-still"));
-    }
-});
+            });
+    });
 });
